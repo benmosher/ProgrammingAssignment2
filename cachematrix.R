360192789f2bfd3da30fd874b97e7ea09dd6a2ca
Ben Mosher <ben@answerrocket.com>
2014-04-27 10:38:28 -0400

    Functions written.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a "matrix" pseudo-object (list-based)
+## and provide a cached 'solve' proxy.
 
-## Write a short comment describing this function
-
+## This function initializes the list for the matrix "object".
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## This function is a proxy for matrix 'solve' function. Arguments are 
+## identical to 'solve', except that a cacheMatrix "object" must be provided
+## instead of a primitive matrix.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+         s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
